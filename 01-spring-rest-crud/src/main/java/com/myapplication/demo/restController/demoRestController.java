ddd04c1197aea8291ac048a13eaac5f2d8bc48ff
Rajesha Hati <rajeshhati777@gmail.com>
2023-06-25 13:42:10 +0530

    first commit

---
+++ 01-spring-rest-crud/src/main/java/com/myapplication/demo/restController/demoRestController.java
@@ -0,0 +1,11 @@
+package com.myapplication.demo.restController;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class demoRestController {
+   public String test(){
+      return "rajesh hati";
+   }
+
+   }
